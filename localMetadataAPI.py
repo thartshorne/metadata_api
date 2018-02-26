ee286adee8163f9c8f020975a46ad9555e359344
Alex Kindel <alextkindel@gmail.com>
2018-02-25 20:24:41 -0500

    Adjust tabs

--- localMetadataAPI.py
+++ localMetadataAPI.py
@@ -2,91 +2,91 @@
 # varDict = OrderedDict()
 # orderedVars = collections.orderedDict(varDict)
 import csv
-import sys  
+import sys
 
-reload(sys)  
+reload(sys)
 sys.setdefaultencoding('latin1')
 import json
 class Metadata:
-	def __init__(self, filename=None):
-		self.metadataDict = {}
-		self.fieldNames = []
-		if filename is None:
-			filename = "FFMetadata20171101_tab.txt"
-		try:
-			with open(filename, "rU") as f:
-				first = 0
-				for line in csv.reader(f, dialect="excel-tab"):
-					if first == 0:
-						self.fieldNames = line
-					else:
-						variable = line
-						index = 0
-						varDict = {}
-						for attr in variable:
-							varDict[self.fieldNames[index]] = attr
-							index+=1
-						self.metadataDict[varDict['new_name']] = varDict
-					first +=1
-		except IOError:
-			return 'Error: Unable to read file'
+    def __init__(self, filename=None):
+        self.metadataDict = {}
+        self.fieldNames = []
+        if filename is None:
+            filename = "FFMetadata20171101_tab.txt"
+        try:
+            with open(filename, "rU") as f:
+                first = 0
+                for line in csv.reader(f, dialect="excel-tab"):
+                    if first == 0:
+                        self.fieldNames = line
+                    else:
+                        variable = line
+                        index = 0
+                        varDict = {}
+                        for attr in variable:
+                            varDict[self.fieldNames[index]] = attr
+                            index+=1
+                        self.metadataDict[varDict['new_name']] = varDict
+                    first +=1
+        except IOError:
+            return 'Error: Unable to read file'
 
-	def select(self, varName, fieldName=None):
-		if fieldName is None:
-			try:
-				fullVar = self.metadataDict[varName]
-				return json.dumps(fullVar)
-			except KeyError: return '{}'
+    def select(self, varName, fieldName=None):
+        if fieldName is None:
+            try:
+                fullVar = self.metadataDict[varName]
+                return json.dumps(fullVar)
+            except KeyError: return '{}'
 
-		if fieldName not in self.fieldNames:
-			return ('Error: Invalid field name')
-		try:
-			varField = self.metadataDict[varName][fieldName]
-			return json.dumps(varField)
-		except KeyError: return '{}'
+        if fieldName not in self.fieldNames:
+            return ('Error: Invalid field name')
+        try:
+            varField = self.metadataDict[varName][fieldName]
+            return json.dumps(varField)
+        except KeyError: return '{}'
 
-	def filter(self, filters=None):
-		if filters is None:
-			results = []
-			for variable in self.metadataDict:
-				v = json.dumps(self.metadataDict[variable], ensure_ascii=False)
-				results.append(v)
-			return results
-		for field in filters:
-			if field not in self.fieldNames:
-				return 'Error: Invalid field name(s)'
+    def filter(self, filters=None):
+        if filters is None:
+            results = []
+            for variable in self.metadataDict:
+                v = json.dumps(self.metadataDict[variable], ensure_ascii=False)
+                results.append(v)
+            return results
+        for field in filters:
+            if field not in self.fieldNames:
+                return 'Error: Invalid field name(s)'
 
-		filteredList = []
-		for variable in self.metadataDict:
-			switch = 0
-			for field, value in filters.iteritems():
-				if self.metadataDict[variable][field] != value:
-					switch = 1
-			if switch == 0:
-				v = json.dumps(self.metadataDict[variable], ensure_ascii=False)
-				filteredList.append(v)
-		if filteredList == []:
-			return "{}"
-		return filteredList
+        filteredList = []
+        for variable in self.metadataDict:
+            switch = 0
+            for field, value in filters.iteritems():
+                if self.metadataDict[variable][field] != value:
+                    switch = 1
+            if switch == 0:
+                v = json.dumps(self.metadataDict[variable], ensure_ascii=False)
+                filteredList.append(v)
+        if filteredList == []:
+            return "{}"
+        return filteredList
 
-	def search(self, query, searchBody = None):
-		if searchBody is None:
-			searchBody = self.metadataDict
-		else:
-			searchList = {}
-			for variable in searchBody:
-				variable = json.loads(variable, encoding='latin1')
-				searchList[variable['new_name']] = variable
-			searchBody = searchList
+    def search(self, query, searchBody = None):
+        if searchBody is None:
+            searchBody = self.metadataDict
+        else:
+            searchList = {}
+            for variable in searchBody:
+                variable = json.loads(variable, encoding='latin1')
+                searchList[variable['new_name']] = variable
+            searchBody = searchList
 
-		searchResults = []
-		for variable in searchBody:
-			if query in searchBody[variable]['varlab'] or query in searchBody[variable]['topic1'] or query in searchBody[variable]['topic1'] or query in searchBody[variable]['q_group_list']:
-				v = json.dumps(searchBody[variable], ensure_ascii=False)
-				searchResults.append(v)
-		if searchResults == []:
-			return "{}"
-		return searchResults
+        searchResults = []
+        for variable in searchBody:
+            if query in searchBody[variable]['varlab'] or query in searchBody[variable]['topic1'] or query in searchBody[variable]['topic1'] or query in searchBody[variable]['q_group_list']:
+                v = json.dumps(searchBody[variable], ensure_ascii=False)
+                searchResults.append(v)
+        if searchResults == []:
+            return "{}"
+        return searchResults
 
 
 
