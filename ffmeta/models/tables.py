c4dbce04e70e29a705a53ae8c76b32165ee536da
Vineet Bansal <vineetbansal@gmail.com>
2018-04-27 16:56:46 -0400

    Removed unused module.

--- ffmeta/models/tables.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import Table, Column, Numeric, Integer, String, ForeignKey, DateTime
-
-from ffmeta.models.db import Base
-response_table = Table('response', Base.metadata, autoload=True)
