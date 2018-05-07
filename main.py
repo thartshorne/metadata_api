9ae2752b936890a48711420b0abb9f5af16e0845
Vineet Bansal <vineetbansal@gmail.com>
2018-05-07 10:40:23 -0400

    Main application object in main.py renamed from 'app' to 'application', to work with mod_wsgi.

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from ffmeta import create_app
 from ffmeta.settings import DEBUG
 from ffmeta.utils import epochalypse_now
 
-app = create_app(debug=DEBUG)
+application = create_app(debug=DEBUG)
 
 
 if __name__ == '__main__':
     # Configure logging (save 10mb of logs in chunks of 1mb)
     handler = RotatingFileHandler('api.log', maxBytes=1024 * 1024, backupCount=10)
     handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info("[{}] App launched.".format(epochalypse_now()))
-    app.run(threaded=True)
+    application.logger.addHandler(handler)
+    application.logger.setLevel(logging.INFO)
+    application.logger.info("[{}] App launched.".format(epochalypse_now()))
+    application.run(threaded=True)
